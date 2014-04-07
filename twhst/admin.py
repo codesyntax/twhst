f2499d4cb791c683041211357ed1ba4a9bb9ca6a
Aitzol Naberan <anaberan@codesyntax.com>
2014-04-07 14:31:59 +0200

    Set autoslug

--- twhst/admin.py
+++ twhst/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from twhst.models import Hashtag
 
+class HashtagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
 # Register your models here.
-admin.site.register(Hashtag)
+admin.site.register(Hashtag, HashtagAdmin)
